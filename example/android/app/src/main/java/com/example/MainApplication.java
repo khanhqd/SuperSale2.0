c92c8fc5e6f404ffcd8f180bf68f35834b09e9a9
Boobosua <nttrung95@yahoo.com>
2016-12-02 03:57:25 +0700

    Thêm khung login và button ( đang cố sửa đừng đụng vào )

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,12 +1,12 @@
 package com.example;
 
 import android.support.annotation.Nullable;
-
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 
 import java.util.List;
-
+import java.util.Arrays;
 public class MainApplication extends NavigationApplication {
     @Override
     public boolean isDebug() {
@@ -16,6 +16,8 @@ public class MainApplication extends NavigationApplication {
     @Nullable
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
-        return null;
+        return Arrays.<ReactPackage>asList(
+            new VectorIconsPackage()
+        );
     }
 }
